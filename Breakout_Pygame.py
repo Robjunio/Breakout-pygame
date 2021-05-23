a4dcb5c2d2adc4d05b84b10946dff9e2bdb9863d
Roberto Gomes <rjrg.snf20@uea.edu.br>
2021-05-23 12:46:10 -0400

    Inserção de vidas
    O jogo agora dispõe de vidas, além de ter uma tela de game over. Outras funcionalidade posta foi os blocos serem recolocados após todos serem quebrados .

--- Breakout_Pygame.py
+++ Breakout_Pygame.py
@@ -1,14 +1,12 @@
 import pygame
-
+from time import sleep
 
 pygame.init()
 
-# Definição da tela
-size = (578, 540) 
-screen = pygame.display.set_mode(size)
-pygame.display.set_caption("Breakout - Pygame Edition")
+MAX_BLOCOS = 84
+blocks_gone = 0
+lifes = 5
 
-# Defining colors
 azul = (66, 72, 200)
 verde = (72, 160, 72)
 amarelo =(162, 162, 42)
@@ -16,6 +14,24 @@ amarelo_escuro = (180, 122, 48)
 laranja = (198, 108, 58)
 vermelho = (200, 72, 72)
 black = (0, 0, 0)
+gray = (142, 142, 142)
+
+# Definição da tela
+size = (578, 540) 
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Breakout - Pygame Edition")
+
+score_font = pygame.font.Font('Sprites/PressStart2P.ttf', 44)
+score_text = score_font.render('5', True, gray, black)
+score_text_rect = score_text.get_rect()
+score_text_rect.center = (289, 36)
+
+game_over = pygame.font.Font('Sprites/PressStart2P.ttf', 44)
+game_over = score_font.render('GAME OVER', True, gray, black)
+game_over_rect = score_text.get_rect()
+game_over_rect.center = (120, 270)
+
+# Defining colors
 
 
 #blocos
@@ -108,72 +124,91 @@ while game_loop:
                 player_move_left = False
             if event.key == pygame.K_RIGHT:
                 player_move_right = False
-                
-    screen.fill(black)
 
-    ball_x += ball_dx
-    ball_y += ball_dy
-    # Player right movement
-    if player_move_right:
-        player_x += 10
-    else:
-        player_x += 0
+    if lifes > 0:            
+        screen.fill(black)
 
-    # Player left movement
-    if player_move_left:
-        player_x -= 10
-    else:
-        player_x += 0   
+        ball_x += ball_dx
+        ball_y += ball_dy
+        # Player right movement
+        if player_move_right:
+            player_x += 10
+        else:
+            player_x += 0
 
-    # Players colision
-    if player_x > 474:  # Right wall
-        player_x = 474
+        # Player left movement
+        if player_move_left:
+            player_x -= 10
+        else:
+            player_x += 0   
 
-    if player_x < 44:  # Left wall
-        player_x = 44
+        # Players colision
+        if player_x > 474:  # Right wall
+            player_x = 474
 
-    # Ball colision
-    if ball_x > 524:  # Right wall
-        if ball_y < 500:
-            ball_x = 524
-            ball_dx *= -1
+        if player_x < 44:  # Left wall
+            player_x = 44
 
-    if ball_x < 44:  # Left wall
-        if ball_y < 500:
-            ball_x = 44
-            ball_dx *= -1
+        # Ball colision
+        if ball_x > 524:  # Right wall
+            if ball_y < 500:
+                ball_x = 524
+                ball_dx *= -1
 
-    if ball_y < 104: # Upper wall
-        ball_y = 104
+        if ball_x < 44:  # Left wall
+            if ball_y < 500:
+                ball_x = 44
+                ball_dx *= -1
+
+        if ball_y < 104: # Upper wall
+            ball_y = 104
+            ball_dy *= -1
+
+        if ball_y > 540:
+            lifes -= 1
+            ball_x = 284
+            ball_y = 284
+            ball_dx = 2.5
+            ball_dy = 2.5
         
-        ball_dy *= -1
-    
-    if 596 > ball_y > 480:
-        if ball_x + 10 > player_x:
-            if player_x + 60 > ball_x: 
-                if 50 > ball_x > 10:
-                    ball_dy *= 1
-                else:
+        if 496 > ball_y > 486:
+            if ball_x + 10 > player_x:
+                if player_x + 60 > ball_x: 
+                    if player_x + 50 >  ball_x > player_x + 10:
+                        ball_dx *= 1.05
+                    else:
+                        ball_dx *= -1.05
+                    ball_y = 486
                     ball_dy *= -1
-                ball_y = 486
-                ball_dx *= -1.1
 
-    for row in wall.blocos:
-        for item in row:
-            if item[0][1] + 10 > ball_y > item[0][1] :
-                if ball_x + 10 > item[0][0]:
-                    if item[0][0] + item[0][2] > ball_x:
-                        ball_y = item[0][1] + 21
-                        ball_dy *= -1
-                        item[0] = (0,0,0,0)
+        for row in wall.blocos:
+            for item in row:
+                if item[0][1] + 20  > ball_y > item[0][1]:
+                    if ball_x + 10 > item[0][0]:
+                        if item[0][0] + item[0][2] > ball_x:  
+                            ball_dy *= -1
+                            item[0] = (0,0,0,0)
+
+                            blocks_gone += 1
                         
-                        
-    screen.blit(back, (0,60))
-    screen.blit(player, (player_x, 496))
-    screen.blit(ball, (ball_x, ball_y))
-        
-    wall.draw_wall()
+        if blocks_gone == MAX_BLOCOS:
+            blocks_gone = 0
+            ball_x = 284
+            ball_y = 284
+            wall.create_wall()
+            sleep(2)
+
+        score_text = score_font.render(str(lifes), True, gray, black)
     
+        screen.blit(back, (0,60))
+        screen.blit(player, (player_x, 496))
+        screen.blit(ball, (ball_x, ball_y))
+        screen.blit(score_text, score_text_rect)
+            
+        wall.draw_wall()
+    else:
+        screen.fill(black)
+        screen.blit(game_over, game_over_rect)
     pygame.display.flip()
     game_clock.tick(60)
 
