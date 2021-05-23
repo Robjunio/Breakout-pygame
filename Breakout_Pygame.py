3ca0828176c67c56cd1c7cf6e72d3d61627e2ba9
Wadrian Araujo de Moura <wadm.snf20@uea.edu.br>
2021-05-23 14:08:33 -0400

    add sounds e melhoria no codigo

--- Breakout_Pygame.py
+++ Breakout_Pygame.py
@@ -3,23 +3,24 @@ from time import sleep
 
 pygame.init()
 
-MAX_BLOCOS = 84
+MAX_BRICKS = 84
 blocks_gone = 0
-lifes = 5
+life = 5
 
-azul = (66, 72, 200)
-verde = (72, 160, 72)
-amarelo =(162, 162, 42)
-amarelo_escuro = (180, 122, 48)
-laranja = (198, 108, 58)
-vermelho = (200, 72, 72)
+BLUE = (66, 72, 200)
+GREEN = (72, 160, 72)
+YELLOW = (162, 162, 42)
+YELLOW2 = (180, 122, 48)
+ORANGE = (198, 108, 58)
+RED = (200, 72, 72)
 black = (0, 0, 0)
 gray = (142, 142, 142)
 
 # Sound
 bounce = pygame.mixer.Sound("bounce.wav")
+lose = pygame.mixer.Sound("lose.wav")
 
-# Definição da tela
+# Screen definition
 size = (578, 540) 
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Breakout - Pygame Edition")
@@ -29,68 +30,64 @@ score_text = score_font.render('5', True, gray, black)
 score_text_rect = score_text.get_rect()
 score_text_rect.center = (289, 36)
 
-game_over = pygame.font.Font('Sprites/PressStart2P.ttf', 44)
-game_over = score_font.render('GAME OVER', True, gray, black)
+game_over_font = pygame.font.Font('Sprites/PressStart2P.ttf', 44)
+game_over = game_over_font.render('GAME OVER', True, gray, black)
 game_over_rect = score_text.get_rect()
 game_over_rect.center = (120, 270)
 
-# Defining colors
 
-
-#blocos
-class wall():
+# Bricks
+class Wall:
     def __init__(self):
+        self.bricks = []
         self.width = (size[0] - 88) // 14
         self.height = 20
 
-    
     def create_wall(self):
-        self.blocos = []
-        bloco_individual = []
-        for linha in range(6):
-            linha_blocos = []
+        self.bricks = []
+        for line in range(6):
+            line_bricks = []
             for col in range(14):
-                bloco_x = 44+ self.width* col
-                bloco_y = 164 + self.height * linha
-                rect = pygame.Rect(bloco_x, bloco_y, self.width, self.height)
+                bricks_x = 44 + self.width * col
+                bricks_y = 164 + self.height * line
+                rect = pygame.Rect(bricks_x, bricks_y, self.width, self.height)
 
-                #força do bloco
-                if linha < 1:
+                # Block strength
+                if line < 1:
                     level = 1
-                elif linha < 2:
+                elif line < 2:
                     level = 2
-                elif linha < 3:
+                elif line < 3:
                     level = 3
-                elif linha < 4:
-                    level  = 4
-                elif linha < 5:
+                elif line < 4:
+                    level = 4
+                elif line < 5:
                     level = 5
-                elif linha < 6:
+                elif line < 6:
                     level = 6
-                bloco_individual = [rect, level]
-                linha_blocos.append(bloco_individual)
-            self.blocos.append(linha_blocos)
-
+                individual_bricks = [rect, level]
+                line_bricks.append(individual_bricks)
+            self.bricks.append(line_bricks)
 
     def draw_wall(self):
-        for linha in self.blocos:
-            for bloco in linha:
-                if bloco[1] == 1:
-                    cor_bloco = vermelho
-                elif bloco[1] == 2:
-                    cor_bloco = laranja
-                elif bloco[1] == 3:
-                    cor_bloco = amarelo_escuro
-                elif bloco[1] == 4:
-                    cor_bloco = amarelo
-                elif bloco[1] == 5:
-                    cor_bloco = verde
-                elif bloco[1] == 6:
-                    cor_bloco = azul
-                pygame.draw.rect(screen, cor_bloco, bloco[0])
+        for line in self.bricks:
+            for brick in line:
+                if brick[1] == 1:
+                    color_brick = RED
+                elif brick[1] == 2:
+                    color_brick = ORANGE
+                elif brick[1] == 3:
+                    color_brick = YELLOW2
+                elif brick[1] == 4:
+                    color_brick = YELLOW
+                elif brick[1] == 5:
+                    color_brick = GREEN
+                elif brick[1] == 6:
+                    color_brick = BLUE
+                pygame.draw.rect(screen, color_brick, brick[0])
 
                
-# player
+# Player
 player = pygame.image.load("Sprites/paddle.png")
 player_x = 269
 player_move_right = False
@@ -103,10 +100,10 @@ ball_y = 284
 ball_dx = 2.5
 ball_dy = 2.5
 
-wall = wall()
+wall = Wall()
 wall.create_wall()
 
-back = pygame.image.load("Sprites/fundo_atari.png")
+back = pygame.image.load("Sprites/background_atari.png")
 
 game_loop = True
 game_clock = pygame.time.Clock()
@@ -128,11 +125,12 @@ while game_loop:
             if event.key == pygame.K_RIGHT:
                 player_move_right = False
 
-    if lifes > 0:            
+    if life > 0:
         screen.fill(black)
 
         ball_x += ball_dx
         ball_y += ball_dy
+
         # Player right movement
         if player_move_right:
             player_x += 10
@@ -145,39 +143,43 @@ while game_loop:
         else:
             player_x += 0   
 
-        # Players colision
+        # Players collision
         if player_x > 474:  # Right wall
             player_x = 474
 
         if player_x < 44:  # Left wall
             player_x = 44
 
-        # Ball colision
+        # Ball collision
         if ball_x > 524:  # Right wall
             if ball_y < 500:
                 ball_x = 524
                 ball_dx *= -1
                 bounce.play()
+
         if ball_x < 44:  # Left wall
             if ball_y < 500:
                 ball_x = 44
                 ball_dx *= -1
                 bounce.play()
-        if ball_y < 104: # Upper wall
+
+        if ball_y < 104:  # Upper wall
             ball_y = 104
             ball_dy *= -1
             bounce.play()
+
         if ball_y > 540:
-            lifes -= 1
+            life -= 1
             ball_x = 284
             ball_y = 284
             ball_dx = 2.5
             ball_dy = 2.5
-            bounce.play()
+            lose.play()
+
         if 496 > ball_y > 486:
             if ball_x + 10 > player_x:
                 if player_x + 60 > ball_x: 
-                    if player_x + 50 >  ball_x > player_x + 10:
+                    if player_x + 50 > ball_x > player_x + 10:
                         ball_dx *= 1.05
                         bounce.play()
                     else:
@@ -186,26 +188,27 @@ while game_loop:
                     ball_y = 486
                     ball_dy *= -1
                     bounce.play()
-        for row in wall.blocos:
+
+        for row in wall.bricks:
             for item in row:
-                if item[0][1] + 20  > ball_y > item[0][1]:
+                if item[0][1] + 20 > ball_y > item[0][1]:
                     if ball_x + 10 > item[0][0]:
                         if item[0][0] + item[0][2] > ball_x:  
                             ball_dy *= -1
-                            item[0] = (0,0,0,0)
+                            item[0] = (0, 0, 0, 0)
                             bounce.play()
                             blocks_gone += 1
                         
-        if blocks_gone == MAX_BLOCOS:
+        if blocks_gone == MAX_BRICKS:
             blocks_gone = 0
             ball_x = 284
             ball_y = 284
             wall.create_wall()
             sleep(2)
 
-        score_text = score_font.render(str(lifes), True, gray, black)
+        score_text = score_font.render(str(life), True, gray, black)
     
-        screen.blit(back, (0,60))
+        screen.blit(back, (0, 60))
         screen.blit(player, (player_x, 496))
         screen.blit(ball, (ball_x, ball_y))
         screen.blit(score_text, score_text_rect)
